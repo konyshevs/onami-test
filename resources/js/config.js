f3da3b9230d34522ca1946509d2bc26d9d926475
konyshevs <konyshevs@gmail.com>
2021-12-31 17:26:50 +0200

    specials on

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,7 +1,5 @@
 export const START_DATE = new Date(2021, 12, 31, 17, 30);
 export const END_DATE = new Date(2022, 1, 1, 2);
 const NOW = new Date();
-export const FIRS_LOADED_PAGE =
-  START_DATE < NOW && END_DATE > NOW ? "specials" : "appetisers";
-export const IS_SPACILALS_BUTTON_ACTIVE =
-  START_DATE < NOW && END_DATE > NOW ? true : false;
+export const FIRS_LOADED_PAGE = "specials";
+export const IS_SPACILALS_BUTTON_ACTIVE = true;
