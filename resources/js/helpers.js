c11ba9aef60e2b5504037f92e7904a55124bef5f
konyshevs <konyshevs@gmail.com>
2022-02-22 14:43:39 +0200

    test commit

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -24,26 +24,26 @@ export const runBeforeDate = function (date, func) {
   }
 };
 
-// export const AJAX = async function (url, uploadData = undefined) {
-//   try {
-//     const fetchPro = uploadData
-//       ? fetch(url, {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify(uploadData),
-//         })
-//       : fetch(url);
+export const AJAX = async function (url, uploadData = undefined) {
+  try {
+    const fetchPro = uploadData
+      ? fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
 
-//     const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
-//     const data = await res.json();
-//     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-//     return data;
-//   } catch (err) {
-//     throw err;
-//   }
-// };
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
 
 const retryConfig = {
   retries: 10,
